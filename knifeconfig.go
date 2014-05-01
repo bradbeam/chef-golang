6fb3e96dc498fa27642a75bf42a9f6bd6b7cde89
Brad Beam <brad.beam@b-rad.info>
2014-04-30 23:03:07 -0500

    Issue-15 updates from PR comments

--- knifeconfig.go
+++ knifeconfig.go
@@ -2,24 +2,28 @@ package chef
 
 import "crypto/rsa"
 
+// chef.KnifeConfig defines all parameters of a knife.rb config file.
+// Most attributes can be mapped up with the parameters in knife.rb
+// by changing the capitalization to _<lowercase>
+// More information can be found at:
 // http://docs.opscode.com/config_rb_knife.html
 type KnifeConfig struct {
-	ChefServerUrl string
-	Host          string
-	Port          string
-	//chef_zero[:enabled]
-	//chef_zero[:port]
-	ClientKey         *rsa.PrivateKey
-	CookbookCopyright string
-	CookbookEmail     string
-	CookbookLicense   string
-	//cookbook_path map[]string
+	ChefServerUrl         string
+	Host                  string
+	Port                  string
+	ChefZeroEnabled       bool
+	ChefZeroPort          string
+	ClientKey             *rsa.PrivateKey
+	CookbookCopyright     string
+	CookbookEmail         string
+	CookbookLicense       string
+	CookbookPath          []string
 	DataBagEncryptVersion int64
 	LocalMode             bool
 	NodeName              string
-	//no_proxy map[]string
-	SyntaxCheckCachePath string
-	ValidationClientName string
-	ValidationKey        string
-	VersionedCookbooks   bool
+	NoProxy               []string
+	SyntaxCheckCachePath  string
+	ValidationClientName  string
+	ValidationKey         string
+	VersionedCookbooks    bool
 }
